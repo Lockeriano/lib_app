57a25ea0c29f410f47931f14e7dfebb731ede42c
Lockeriano <patryk.skrzyniarz19@gmail.com>
2015-12-13 14:37:54 +0000

    Updated book database

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20151210125553) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
+    t.string   "author"
     t.string   "species"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
