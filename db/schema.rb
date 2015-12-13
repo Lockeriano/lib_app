0e57103626b7dbb0be30ca05c7e4828e7f7fec26
Lockeriano <patryk.skrzyniarz19@gmail.com>
2015-12-13 15:47:59 +0000

    Users and tests done

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210125553) do
+ActiveRecord::Schema.define(version: 20151213151525) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
-    t.string   "author"
     t.string   "species"
+    t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
