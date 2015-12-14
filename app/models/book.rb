0112c3320ee6d211588c3ec74f09421b5a10b15e
nightreapper <darobiel@gmail.com>
2015-12-14 22:08:00 +0100

    adding categories to books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ActiveRecord::Base
+    belongs_to :category
 	validates :title, presence: true
-	validates :author, presence: true
 	validates :species, presence: true
+    validates :author, presence: true
+    validates :category_id, presence: true
 end
