01ef178d7a538839a910fa18ab75d7a31e7d2f74
nightreapper <darobiel@gmail.com>
2015-12-16 22:22:33 +0100

    path reindent

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,11 @@
 class Book < ActiveRecord::Base
-    belongs_to :category
-	validates :title, presence: true
-	validates :species, presence: true
-    validates :author, presence: true
-    validates :category_id, presence: true
+  belongs_to :category
+  validates :title, presence: true
+  validates :species, presence: true
+  validates :author, presence: true
+  validates :category_id, presence: true
 
-    def category_name
-      self.category.name
-    end
+  def category_name
+    self.category.name
+  end
 end
