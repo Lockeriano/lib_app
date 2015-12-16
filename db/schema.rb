8b24d64ded2bdfcdd5c2e3935d36a0ef96a7884f
Lockeriano <patryk.skrzyniarz19@gmail.com>
2015-12-16 22:34:24 +0000

    master

--- db/schema.rb
+++ db/schema.rb
@@ -28,4 +28,12 @@ ActiveRecord::Schema.define(version: 20151213234954) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
 end
